e88dd2d97390f0f52c06541013277a44de8e1023
Andrew Braun <andrew@andrewbraun.dev>
2020-12-09 17:54:23 +0400

    fix: Resolve issue with mobile menu on small vertical screen sizes

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,12 @@
 import React from "react"
 import SocialButtons from "../social-buttons/SocialButtons"
+import "../../styles/global.css"
 import styles from "./footer.module.css"
 
 export default function Footer() {
 	return (
 		<footer className={styles.mainFooter}>
-            <SocialButtons />
+			<SocialButtons />
 		</footer>
 	)
 }
